c5ba130063190f2c5d5248fa52259e3f11d64894
Vincent C <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2024-10-01 17:15:39 +0200

    fix 3.2 and 3.3

--- Groupe1/TP3/src/repl.c
+++ Groupe1/TP3/src/repl.c
@@ -4,67 +4,148 @@
 char* VERSION = "1.0";
 int CONTINUE = 1;
 
-struct Commande {
-    char* nom;
-    void (*fonction)();
-    char* description;
+enum lang {
+    FR,
+    EN
 };
+
+typedef enum lang Lang;
+
+typedef struct {
+    char* nom_FR;
+    char* nom_EN;
+} Nom;
+
+// Structure pour la description
+typedef struct {
+    char* description_FR;
+    char* description_EN;
+} Description;
+
+// Structure pour les commandes
+typedef struct {
+    Nom nom;
+    Description description;
+    void (*fonction)(Lang, char*);
+} Commande;
 
 // Function declarations
-void traiter_echo(char commande[1024]);
-void traiter_quit();
-void afficher_aide();
-void afficher_version();
-void afficher_date();
+void traiter_echo(Lang lang, char* commande);
+void traiter_quit(Lang lang, char* unused);
+void afficher_aide(Lang lang, char* unused);
+void afficher_version(Lang lang, char* unused);
+void corriger_commande(char* commande);
+void afficher_date(Lang lang, char* unused);
 
 //tableau de commandes
-struct Commande commandes[] = {
-    {"echo", traiter_echo, "Afficher du texte dans l'interpréteur"},
-    {"quit", traiter_quit, "Quitter l'interpréteur"},
-    {"quitter", traiter_quit, "Quitter l'interpréteur"},
-    {"help", afficher_aide, "Afficher l'aide de l'interpréteur"},
-    {"aide", afficher_aide, "Afficher l'aide de l'interpréteur"},
-    {"version", afficher_version, "Afficher la version de l'interpréteur"},
-    {"date", afficher_date, "Afficher la date et l'heure actuelles"}
+Commande commandes[] = {
+    {
+        {"echo","echo"},
+        {"Afficher du texte dans l'interpréteur", "Display text in the REPL"},
+        traiter_echo},
+    {
+        {"quitter","quit"},
+        {"Quitter l'interpréteur", "Exit the REPL"},
+        traiter_quit},
+    {
+        {"aide","help"},
+        {"Afficher l'aide de l'interpréteur", "Display the help of the REPL"},
+        afficher_aide},
+    {
+        {"version","version"},
+        {"Afficher la version de l'interpréteur", "Display the version of the REPL"},
+        afficher_version},
+    {
+        {"date","date"},
+        {"Afficher la date et l'heure actuelles", "Display the current date and time"},
+        afficher_date},
+    {
+        {NULL, NULL},
+        {NULL, NULL},
+        NULL}
+
 };
 
-void traiter_echo(char commande[1024]) {
+void traiter_echo(Lang lang, char* commande) {
     // Traite la commande "echo" pour afficher du texte
-    printf("Echo: ");
-
-    // Imprime la chaîne
-    for (int i = 5; commande[i] != '\0'; i++)
-    {
-        printf("%c", commande[i]);
+    if (commande[5] == '\0') {
+        printf("%s\n", lang == FR ? "Erreur: la commande 'echo' nécessite un argument" : "Error: 'echo' command requires an argument");
+    } else {
+        printf("%s\n", lang == FR ? "Texte à afficher:" : "Text to display:");
+        printf("%s\n", &commande[5]);
     }
-    printf("\n"); // Saut de ligne après la sortie
 }
 
-void traiter_quit() {
-    // Quitte le programme si la commande est "quit"
-    printf("Arrêt...\n");
+void traiter_quit(Lang lang, char* unused) {
+    printf("%s\n", lang == FR ? "Au revoir!" : "Goodbye!");
     CONTINUE = 0;
 }
 
-void afficher_aide() {
-    printf("Liste des commandes:\n");
-    for (int i = 0; i < sizeof(commandes) / sizeof(commandes[0]); i++) {
-        printf("%s : %s\n", commandes[i].nom, commandes[i].description);
+void afficher_aide(Lang lang, char* unused) {
+    printf("%s\n", lang == FR ? "Liste des commandes:" : "List of commands:");
+    for (int i = 0; commandes[i].nom.nom_FR != NULL; i++) {
+        printf("%s: %s\n", lang == FR ? commandes[i].nom.nom_FR : commandes[i].nom.nom_EN,
+               lang == FR ? commandes[i].description.description_FR : commandes[i].description.description_EN);
     }
 }
 
-void afficher_version() {
-    printf("version: %s", VERSION);
+void afficher_version(Lang lang, char* unused) {
+    printf("%s: %s\n", lang == FR ? "Version" : "Version", VERSION);
 }
 
-void afficher_commande_vide() {
-   printf("Commande vide, veuillez entrer une commande.\n");
+void afficher_mauvaise_commande(Lang lang, char* commande) {
+        corriger_commande(commande);
 }
 
-void afficher_date()
-{
-    // Affiche la date et l'heure actuelles
-    printf("Date et heure actuelles: " __DATE__ " " __TIME__ "\n");
+void corriger_commande(char* commande) {
+    // Donne la commande ressemblant le plus à la commande entrée
+    int min = 100;
+    char* commande_corrige = NULL;
+    Lang lang = FR;
+    for (int i = 0; commandes[i].nom.nom_FR != NULL; i++) {
+        int diff = strcmp(commandes[i].nom.nom_FR, commande);
+        if (diff < min) {
+            min = diff;
+            commande_corrige = commandes[i].nom.nom_FR;
+        }
+        diff = strcmp(commandes[i].nom.nom_EN, commande);
+        if (diff < min) {
+            min = diff;
+            commande_corrige = commandes[i].nom.nom_EN;
+            lang = EN;
+        }
+    }
+    printf("%s %s %s %s\n",
+    lang == FR ? "Commande non reconnue, voulez-vous dire" : "Command not recognized, did you mean",
+    commande_corrige, "? \n",
+    lang == FR ? "Essayez 'aide' pour obtenir de l'aide." : "Try 'help' for help.");
+}
+
+void afficher_date(Lang lang, char* unused) {
+    printf("%s " __DATE__ " " __TIME__, lang == FR ? "Date et heure actuelles:" : "Current date and time:");
+}
+
+char* set_commande(){
+    static char commande[1024];
+
+    // Lit la commande utilisateur et la stocke dans le buffer
+    fgets(commande, sizeof(commande), stdin);
+
+    // Enlève le caractère de fin de ligne ajouté par fgets
+    commande[strcspn(commande, "\n")] = 0;
+
+    return commande;
+}
+
+void traiter_commande(char* commande){
+    for (int i = 0; commandes[i].nom.nom_FR != NULL; i++) {
+        if (strcmp(commandes[i].nom.nom_FR, commande) == 0 || strcmp(commandes[i].nom.nom_EN, commande) == 0) {
+            commandes[i].fonction(strcmp(commandes[i].nom.nom_FR, commande) == 0 ? FR : EN, commande);
+            return;
+        }
+    }
+    // Affiche un message d'erreur si la commande n'est pas reconnue, FR est par défaut mais dans tout les cas, le message est dans les deux langues
+    afficher_mauvaise_commande(FR, commande);
 }
 
 
@@ -79,53 +160,14 @@ int main()
     // Boucle principale qui lit et traite les commandes utilisateur
     while (CONTINUE)
     {
-        printf("> "); // Affiche le prompt de commande
+        printf("\n> "); // Affiche le prompt de commande
 
         // Buffer pour stocker la commande utilisateur
-        char commande[1024];
-
-        // Lit la commande utilisateur et la stocke dans le buffer
-        fgets(commande, sizeof(commande), stdin);
-
-        // Enlève le caractère de fin de ligne ajouté par fgets
-        commande[strcspn(commande, "\n")] = 0;
+        char* commande = set_commande();
 
         // Traite la commande en fonction de son contenu
-        if (strcmp(commande, "quit") == 0)
-        {
-            traiter_quit();
-        }
-        else if (strcmp(commande, "quitter") == 0){
-            traiter_quit();
-        }
-        else if (strncmp(commande, "echo ", 5) == 0)
-        {
-            traiter_echo(commande);
-        }
-        else if (strcmp(commande, "help") == 0) {
-            afficher_aide();
-        }
-        else if (strcmp(commande, "aide") == 0) {
-            afficher_aide();
-        }
-        else if (strcmp(commande, "version") == 0) {
-            afficher_version();
-        }
-        else if (strcmp(commande, "date") == 0)
-        {
-           afficher_date();
-        }
-        else if (strcmp(commande, "") == 0)
-        {
-           afficher_commande_vide();
-        }
-        else
-        {
-            // Affiche un message d'erreur si la commande est inconnue
-            printf("Commande non reconnue. Essayez 'echo <text>' pour afficher du texte, ou tapez 'quit' pour quitter.\n");
-        }
+        traiter_commande(commande);
 
-        printf("\n"); // Saut de ligne après la sortie
     }
 
     return 0;
