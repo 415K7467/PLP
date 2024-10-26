e0f135f392bfd461adaa42a64616a3d5b609b3f8
Vincent C <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2024-10-26 17:53:19 +0200

    fix: 4.3 ajout du système de langue pour les calculs

--- Groupe1/TP4/src/repl.h
+++ Groupe1/TP4/src/repl.h
@@ -5,7 +5,5 @@ extern int CONTINUE;
 
 char* set_commande();
 void traiter_commande(char* commande);
-void handleInfixExpression(const char* expression);
-void handlePostfixExpression(const char* expression);
 
 #endif // REPL_H
